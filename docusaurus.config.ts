a2a3564b1586e8c46fbb42c86778ad9744d4288d
tushengtao <1450190944@qq.com>
2024-07-09 23:26:27 +0800

    deploy config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'TuShengTao', // Usually your GitHub org/user name.
   projectName: 'TuShengTao.github.io', // Usually your repo name.
-  deploymentBranch:'main',
+  deploymentBranch:'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
