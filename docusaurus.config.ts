4c9591bfdc6db77140ad82fbefc4283787aa6f7f
tuthengtao <1450190944@qq.com>
2024-09-11 14:50:40 +0800

    editUrl update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -58,14 +58,14 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/tushengtao/tushengtao.github.io/',
+            'https://github.com/tushengtao/tushengtao.github.io/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/tushengtao/tushengtao.github.io/',
+            'https://github.com/tushengtao/tushengtao.github.io/tree/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
